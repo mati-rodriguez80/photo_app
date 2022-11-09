382ee02a04808bf9fbf1f1f7fc728b8512c0c9f2
mati-rodriguez80 <95318985+mati-rodriguez80@users.noreply.github.com>
2022-11-09 20:59:02 -0300

    JS file to handle cc info and extend devise registrations contr to create payments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import "packs/credit_card_form"
 
 Rails.start()
 Turbolinks.start()
